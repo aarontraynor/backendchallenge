97b85e70abb9412e37856fce436c5f62a42752f1
Aaron Traynor <aaron.traynor@outlook.com>
2019-08-13 14:32:29 +0100

    Create DriverSerializer

--- carmanagement_api/serializers.py
+++ carmanagement_api/serializers.py
@@ -25,3 +25,15 @@ class BranchSerializer(serializers.ModelSerializer):
                 'read_only': True
             }
         }
+
+class DriverSerializer(serializers.ModelSerializer):
+    """Serializes a driver object"""
+
+    class Meta:
+        model = models.Driver
+        fields = ('id', 'first_name', 'middle_names', 'last_name', 'date_of_birth')
+        extra_kwargs = {
+            'id': {
+                'read_only': True
+            }
+        }
