f4bafad126c8d2d2b0e3e2ad0b0528b5d266b26b
Aaron Traynor <aaron.traynor@outlook.com>
2019-08-13 11:06:32 +0100

    Update settings.py to include REST framework and the newly created Django app

--- carmanagement_project/settings.py
+++ carmanagement_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'carmanagement_api',
 ]
 
 MIDDLEWARE = [
