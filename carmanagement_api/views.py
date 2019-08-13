e562e6a4da0b1edca70230483db224296cbafd7d
Aaron Traynor <aaron.traynor@outlook.com>
2019-08-13 14:24:32 +0100

    Remove TestViewSet

--- carmanagement_api/views.py
+++ carmanagement_api/views.py
@@ -7,14 +7,6 @@ from carmanagement_api import serializers
 from carmanagement_api import models
 
 
-class TestViewSet(viewsets.ViewSet):
-    """API ViewSet for Cars in the system"""
-
-    def list(self, request):
-        """Return an initial message"""
-
-        return Response({'message': 'Hello!'})
-
 class CarViewSet(viewsets.ModelViewSet):
     """Handle creating viewing and updating cars in the system"""
 
