ed32af0b1529cc98544625635877a05382a2bb6a
Aaron Traynor <aaron.traynor@outlook.com>
2019-08-13 13:10:50 +0100

    Add __str__ functions to get String representations of objects

--- carmanagement_api/models.py
+++ carmanagement_api/models.py
@@ -8,12 +8,22 @@ class Branch(models.Model):
     city = models.CharField(max_length=50)
     postcode = models.CharField(max_length=8)
 
+    def __str__(self):
+        """Return a String representation of the branch"""
+        return city + ", " + postcode
+
+
 class Driver(models.Model):
     """Database model for drivers in the system"""
     first_name = models.CharField(max_length=50)
     middle_names = models.CharField(max_length=255)
     last_name = models.CharField(max_length=50)
     date_of_birth = models.DateField()
+
+    def __str__(self):
+        """Return a String representation of the driver"""
+        return self.first_name + " " + self.middle_names + " " + self.last_name
+
 
 class Car(models.Model):
     """Database model for cars in the system"""
@@ -26,3 +36,7 @@ class Car(models.Model):
     at_branch = models.BooleanField(default=True)
     branch = models.ForeignKey(Branch, on_delete=models.SET_NULL, null=True)
     driver = models.ForeignKey(Driver, on_delete=models.SET_NULL, null=True)
+
+    def __str(self):
+        """Return a String representation of the car"""
+        return self.car_make + " " + self.car_model + ", " + year_of_manufacture
