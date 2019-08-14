60fa2810f807601bd1c17c2983725e613f7d1f16
Aaron Traynor <aaron.traynor@outlook.com>
2019-08-14 15:02:56 +0100

    Enable filtering for cars, branches and drivers

--- carmanagement_api/views.py
+++ carmanagement_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework import filters
 
 from carmanagement_api import serializers
 from carmanagement_api import models
@@ -12,18 +13,24 @@ class CarViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.CarSerializer
     queryset = models.Car.objects.all()
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('make', 'model', 'year_of_manufacture')
 
 class BranchViewSet(viewsets.ModelViewSet):
     """Handle creating, viewing and updating branches in the system"""
 
     serializer_class = serializers.BranchSerializer
     queryset = models.Branch.objects.all()
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('city', 'postcode')
 
 class DriverViewSet(viewsets.ModelViewSet):
     """Handle creating, viewing and updating drivers in the system"""
 
     serializer_class = serializers.DriverSerializer
     queryset = models.Driver.objects.all()
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('first_name', 'middle_names', 'last_name', 'date_of_birth')
 
 class BranchInventoryViewSet(viewsets.ModelViewSet):
     """Handle creating, viewing and updating associations between cars and branches"""
