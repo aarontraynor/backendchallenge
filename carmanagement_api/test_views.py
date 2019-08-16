ad7a525aa0c01471e678b47b882d4fafee2c4649
Aaron Traynor <aaron.traynor@outlook.com>
2019-08-16 11:39:21 +0100

    Create test_views.py

---
+++ carmanagement_api/test_views.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+from carmanagement_api.views import CarViewSet, BranchViewSet, DriverViewSet, BranchInventoryViewSet, DriverInventoryViewSet
+
+
+class CarViewSetTestCase(TestCase):
+    def setUp(self):
+        # TODO: Add setup and tests
+
+
+class BranchViewSetTestCase(TestCase):
+    def setUp(self):
+        # TODO: Add setup and tests
+
+
+class DriverViewSetTestCase(TestCase):
+    def setUp(self):
+        # TODO: Add setup and tests
+
+
+class BranchInventoryViewSetTestCase(TestCase):
+    def setUp(self):
+        # TODO: Add setup and tests
+
+
+class DriverInventoryViewSetTestCase(TestCase):
+    def setUp(self):
+        # TODO: Add setup and tests
